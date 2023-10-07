c0bff216e7229b2774b8b1875c02e21d0072ff08
Luis Alejandro Portada Tello <129618534+LuisAlejandro10@users.noreply.github.com>
2023-10-07 12:50:52 -0600

    Add files via upload

---
+++ cadenas.py
@@ -0,0 +1,32 @@
+# Ejercicio 1
+# nombre = input("Escribe tu nombre: ")
+# n = int(input("Escribe un numero: "))
+# print((nombre + "\n") * int(n))
+
+# Ejercicio 2
+# nombre = input("Escribe tu nombre completo: ")
+# print(nombre.upper())
+# print(nombre.lower())
+# print(nombre.title())
+
+# Ejercicio 3
+# print(f"{nombre} tiene {n} letras")
+
+# Ejercicio 4
+# telefono = input("Introduce un número de teléfono con el formato +xx-xxxxxxxxx-xx: ")
+# print('El número de teléfono es ', telefono[1:-3]) #´[1: es lo que se tiene que eliminar y -3 es los que tiene que retomar]
+
+# Ejercico 5
+# rase = input("Escribe tu fraze: ")
+# print(frase[::-1])
+
+# Ejercicio 6
+# frase = input("introduce una frasa: ")
+# vocal = input("introduce una vocal: ")
+# print(frase.replace(vocal, vocal.upper()))
+
+# Ejercicio 7
+# correo = input("Escribe tu correo electronico: ")
+# print(correo[:correo.find('@')] + "@ceu.es")
+# Ejercicio 8
+price_of_the_product = float(input("Price of the prduct: "))
